6f791e1961bb5b07b5efd901cc2e082c9d612b29
Nicholas Neumann-Chun <nicholas.babelthuap@gmail.com>
2015-11-25 21:11:21 -0800

    fixed accept headers

--- server.js
+++ server.js
@@ -21,9 +21,9 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods: GET, POST, OPTIONS, DELETE');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
